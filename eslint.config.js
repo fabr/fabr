fa77410977386b32161a412d4b333665198c8260
Nathan Keynes <nkeynes@deadcoderemoval.net>
2024-05-02 20:47:56 +1000

    Update eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,10 @@
-module.exports = {
-  extends: [
-    "plugin:@typescript-eslint/recommended",
-    "prettier",
-  ],
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
-  settings: {
-    "import/parsers": {
-      "@typescript-eslint/parser": [".ts"],
-    },
-    "import/resolver": {
-      typescript: {},
-    },
-  },
-  rules: {
+const tslint = require("typescript-eslint");
+const eslint = require("@eslint/js");
+
+module.exports = tslint.config(
+  eslint.configs.recommended,
+  ...tslint.configs.recommended,
+  {rules: {
     "@typescript-eslint/no-empty-function": 0,
     "@typescript-eslint/no-this-alias": 0,
     "@typescript-eslint/indent": 0,
@@ -21,5 +12,5 @@ module.exports = {
     "@typescript-eslint/no-inferrable-types": 0,
     "@typescript-eslint/no-namespace": 0,
     "@typescript-eslint/no-non-null-assertion": 0,
-  },
-};
+  }},
+);
